ac597ef97e1e3096f0b58e5847a4b1d3c8c22c4c
Fredrik Wildhagen <github@wildhagen.net>
2025-10-02 22:16:15 +0200

    Moved dotfiles and fixed path

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 DOTFILES=(
-    "hypr/autostart.conf"
-    "hypr/bindings.conf"
-    "hypr/envs.conf"
-    "hypr/hypridle.conf"
-    "hypr/hyprland.conf"
-    "hypr/hyprlock.conf"
-    "hypr/hyprsunset.conf"
-    "hypr/input.conf"
-    "hypr/looknfeel.conf"
-    "hypr/monitors.conf"
+    ""
+    ".config/hypr/autostart.conf"
+    ".config/hypr/bindings.conf"
+    ".config/hypr/envs.conf"
+    ".config/hypr/hypridle.conf"
+    ".config/hypr/hyprland.conf"
+    ".config/hypr/hyprlock.conf"
+    ".config/hypr/hyprsunset.conf"
+    ".config/hypr/input.conf"
+    ".config/hypr/looknfeel.conf"
+    ".config/hypr/monitors.conf"
 )
 
 link_dotfile() {
     local dotfile="$1"
-    local home_path="$HOME/.config/$dotfile"
-    local omarchy_path="$HOME/omarchy/dotfile/$dotfile"
+    local home_path="$HOME/$dotfile"
+    local omarchy_path="$HOME/omarchy/dotfiles/$dotfile"
 
     ln -sf "$omarchy_path" "$home_path"
     echo "Linked: $home_path → $omarchy_path"
