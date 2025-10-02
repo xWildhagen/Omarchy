8d81e11450704a9c08d111eb1ff03f6b702a9321
Fredrik Wildhagen <github@wildhagen.net>
2025-10-02 14:48:01 +0200

    Added list of packages as variables

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+PACMAN_PACKAGEDS=(
+    "visual-studio-code-bin"
+)
+
+YAY_PACKAGES=(
+    "microsoft-edge-stable-bin"
+)
+
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S --noconfirm --needed visual-studio-code-bin
+sudo pacman -S --noconfirm --needed "${PACMAN_PACKAGES[@]}"
 
-yay -S --noconfirm --needed microsoft-edge-stable-bin
+yay -S --noconfirm --needed "${YAY_PACKAGES[@]}"
